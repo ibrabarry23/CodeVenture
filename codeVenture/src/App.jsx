c9c401d5c33c51b06f8d81da3cf20108270cbe35
Luciano <lunick997@gmail.com>
2023-09-25 13:19:57 +0200

    add: ButtonPlay

--- codeVenture/src/App.jsx
+++ codeVenture/src/App.jsx
@@ -3,10 +3,17 @@ import './App.css'
 import Footer from './Components/Footer'
 import RiquadroEsempi from './RiquadroEsempi/RiquadroEsempi'
 import { FramePresentazione } from './Components/FramePresentazione/FramePresentazione'
+import Button from './Components/ButtonPlay/Button'
+import { Routes,Route} from 'react-router-dom'
+import Game from './Components/Game'
 
 function App() {
   return (
     <div className='flex flex-col items-center'>
+      <Button/>
+      <Routes>
+        <Route path='/Game' element={<Game/>} />
+      </Routes>
         <NavBar/>
         <FramePresentazione/>
         <RiquadroEsempi/>
