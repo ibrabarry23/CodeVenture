4f0b08a9ebf88874e644845469e3262b917d2431
Mirka <mirkamedaglini@hotmail.it>
2023-09-29 19:18:22 +0200

    Texture homepage

--- codeVenture/src/Components/FramePresentazione/FramePresentazione.jsx
+++ codeVenture/src/Components/FramePresentazione/FramePresentazione.jsx
@@ -1,7 +1,22 @@
-export const FramePresentazione=()=>{
-    return(
-        <div className="flex justify-center p-12">
-            <video className="w-2/3 rounded-3xl" controls src="./src/assets/video/presentazioneCodeVenture.mp4"></video>
-        </div>
-    )
+import React from 'react'
+import './FramePresentazione.css'
+import imageBorder from '../../assets/image/Screenshot_2023-09-29_alle_17.59.12__2_-removebg-preview.png'
+import imageBg1 from '../../assets/image/Screenshot_2023-09-29_alle_18.23.38__2_-removebg-preview.png'
+import imageBg2 from '../../assets/image/IMG_4255.png'
+
+export const FramePresentazione = () => {
+  return (
+    <div className='frame-container'>
+      <img src={imageBorder} alt='Border' className='border-image' />
+      <img src={imageBg1} alt='Background1' className='bg-image left' />
+      <img src={imageBg2} alt='Background2' className='bg-image right' />
+      <div className='flex justify-center p-12'>
+        <video
+          className='video w-2/3 rounded-3xl'
+          controls
+          src='./src/assets/video/presentazioneCodeVenture.mp4'
+        ></video>
+      </div>
+    </div>
+  )
 }
--- codeVenture/src/RiquadroEsempi/RiquadroEsempi.jsx
+++ codeVenture/src/RiquadroEsempi/RiquadroEsempi.jsx
@@ -1,46 +1,62 @@
-import { useState } from "react"
+import { useState } from 'react'
+import imageLine2 from '../assets/image/Screenshot_2023-09-29_alle_18.45.23__2_-removebg-preview.png'
+import imageBgOne from '../assets/image/Screenshot_2023-09-29_alle_18.54.47__2_-removebg-preview.png'
+import imageBgTwo from '../assets/image/Screenshot_2023-09-29_alle_18.56.22__2_-removebg-preview.png'
 
-function RiquadroEsempio(){
-    const [src, setSrc] = useState("./src/assets/image/image1.jpg");
-    const [activeButton, setActiveButton] = useState("ESEMPIO 1");
+function RiquadroEsempio() {
+  const [src, setSrc] = useState('./src/assets/image/image1.jpg')
+  const [activeButton, setActiveButton] = useState('ESEMPIO 1')
 
-    const buttonColorPress = "w-1/4 h-8 verdeScuro rounded-xl pixelFont font-bold";
-    const buttonColorHold = "w-1/4 h-8 verdeChiaro rounded-xl pixelFont font-bold";
+  const buttonColorPress = 'w-1/4 h-8 verdeScuro rounded-xl pixelFont font-bold'
+  const buttonColorHold = 'w-1/4 h-8 verdeChiaro rounded-xl pixelFont font-bold'
 
-    const handlerClickImg = (e, imgSrc) => {
-        setActiveButton(e.target.innerText);
-        setSrc(imgSrc);
-    };
+  const handlerClickImg = (e, imgSrc) => {
+    setActiveButton(e.target.innerText)
+    setSrc(imgSrc)
+  }
 
-    const getButtonClassName = (buttonText) => {
-        return buttonText === activeButton ? buttonColorPress : buttonColorHold;
-    };
+  const getButtonClassName = (buttonText) => {
+    return buttonText === activeButton ? buttonColorPress : buttonColorHold
+  }
 
-    return(
-        <div className="flex flex-col gap-4 p-5 rounded-xl w-2/4	 ">
-            <div className=" flex justify-around">
-                <button
-                    className={getButtonClassName("ESEMPIO 1")}
-                    onClick={(e) => handlerClickImg(e, "./src/assets/image/image2.jpg")}
-                >
-                    ESEMPIO 1
-                </button>
-                <button
-                    className={getButtonClassName("ESEMPIO 2")}
-                    onClick={(e) => handlerClickImg(e, "./src/assets/image/image1.jpg")}
-                >
-                    ESEMPIO 2
-                </button>
-                <button
-                    className={getButtonClassName("ESEMPIO 3")}
-                    onClick={(e) => handlerClickImg(e, "./src/assets/image/image3.jpg")}
-                >
-                    ESEMPIO 3
-                </button>
-            </div>
-            <img src={src} className=" rounded-xl bg-green-600 w-full" alt="" />
-        </div>
-    )
+  return (
+    <div className='flex flex-col gap-4 p-5 rounded-xl  mx-auto'>
+      <img
+        src={imageLine2}
+        className='w-80 h-auto self-start mt-4'
+        alt='Image Line 2'
+      />
+      <div className='flex justify-around'>
+        <button
+          className={getButtonClassName('ESEMPIO 1')}
+          onClick={(e) => handlerClickImg(e, './src/assets/image/image2.jpg')}
+        >
+          ESEMPIO 1
+        </button>
+        <button
+          className={getButtonClassName('ESEMPIO 2')}
+          onClick={(e) => handlerClickImg(e, './src/assets/image/image1.jpg')}
+        >
+          ESEMPIO 2
+        </button>
+        <button
+          className={getButtonClassName('ESEMPIO 3')}
+          onClick={(e) => handlerClickImg(e, './src/assets/image/image3.jpg')}
+        >
+          ESEMPIO 3
+        </button>
+      </div>
+      <div className='relative flex items-center justify-between'>
+        <img src={imageBgOne} className='w-1/6 rounded-xl' alt='Image BG One' />
+        <img
+          src={src}
+          className='rounded-xl bg-green-600 w-2/3'
+          alt='Current Image'
+        />
+        <img src={imageBgTwo} className='w-1/6 rounded-xl' alt='Image BG Two' />
+      </div>
+    </div>
+  )
 }
 
-export default RiquadroEsempio;
+export default RiquadroEsempio
